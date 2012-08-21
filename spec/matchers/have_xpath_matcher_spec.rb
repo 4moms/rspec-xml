5e46526cead1f2b6b114d3dfac062168b41edcc6
Dan Carper <dcarper@dreamagile.com>
2012-08-21 17:41:29 -0400

    unit tests for RSpecXML::XMLMatchers::HaveXPath

---
+++ spec/matchers/have_xpath_matcher_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+module Factory
+  def self.xpath_matcher(xpath)
+    RSpecXML::XMLMatchers::HaveXPath.new(xpath)
+  end
+end
+
+describe RSpecXML::XMLMatchers::HaveXPath do
+  describe '#intialize' do
+    it 'should build and save a matcher containing the supplied xpath' do
+      
+      RSpecXML::XMLMatchers::HaveXPath::Matcher.
+        expects(:new).
+        with(:xpath => 'fake xpath').
+        returns(:flag)
+
+      xpath_matcher = Factory.xpath_matcher('fake xpath')
+      xpath_matcher.send(:matcher).should == :flag
+
+    end
+  end
+
+  describe '#with_text' do
+    it 'should build a new text matcher containing supplied xpath and text' do
+
+      RSpecXML::XMLMatchers::HaveXPath::TextMatcher.
+        expects(:new).
+        with(:xpath => 'fake xpath', :text => 'blah').
+        returns(:flag)
+
+      xpath_matcher = Factory.xpath_matcher('fake xpath').with_text('blah')
+      xpath_matcher.send(:matcher).should == :flag
+
+    end
+  end
+
+  describe '#failure_message_for_should' do
+    it 'should delegate to the matcher' do
+      xpath_matcher = Factory.xpath_matcher('whatever')
+      xpath_matcher.stubs(:matcher).
+        returns(stub( :failure_message_for_should => 'woo!' ))
+
+      xpath_matcher.failure_message_for_should.should == 'woo!'
+    end
+  end
+
+  describe '#failure_message_for_should' do
+    it 'should delegate to the matcher' do
+      xpath_matcher = Factory.xpath_matcher('whatever')
+      xpath_matcher.stubs(:matcher).
+        returns(stub( :failure_message_for_should_not => 'woo!' ))
+
+      xpath_matcher.failure_message_for_should_not.should == 'woo!'
+    end
+  end
+end
